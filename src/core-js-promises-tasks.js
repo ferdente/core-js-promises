27518b32df984d12dcfd5d1d669d0586425d0b81
Kirk Snow <ferdenal@gmail.com>
2024-03-22 20:39:32 +0300

    feat: add solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -38,8 +38,8 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source.then(() => 'success').catch(() => 'fail');
 }
 
 /**
@@ -55,8 +55,16 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.all(
+    promises.map((p) => p.catch(() => new Error('rejected')))
+  ).then((results) => {
+    const firstResolved = results.find((result) => !(result instanceof Error));
+    if (firstResolved !== undefined) {
+      return firstResolved;
+    }
+    throw new Error('All promises were rejected');
+  });
 }
 
 /**
